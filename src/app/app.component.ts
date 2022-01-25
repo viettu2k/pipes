9786a63af7786736d87baf222e9cc9a0db978ef9
viettu2k <doanviettu2k@gmail.com>
2022-01-25 21:34:42 +0700

    adding a date picker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name = '';
+  date = '';
 
   onNameChange(value: string) {
     this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
   }
 }
